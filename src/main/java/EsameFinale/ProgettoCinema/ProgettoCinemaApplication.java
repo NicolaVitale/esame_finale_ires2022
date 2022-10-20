fcf13e0b87678a22bee11b0c984b7d4ce0841071
Nicola Vitale <nico.vits@gmail.com>
2022-10-20 09:34:15 +0200

    first commit

---
+++ src/main/java/EsameFinale/ProgettoCinema/ProgettoCinemaApplication.java
@@ -0,0 +1,13 @@
+package EsameFinale.ProgettoCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EsameFinale/ProgettoCinema/ProgettoCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package EsameFinale.ProgettoCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
